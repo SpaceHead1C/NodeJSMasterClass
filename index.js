9930b1cee9fd27cbdbdd12eecc1d8beb5d416225
SpaceHead1C <damh17@gmail.com>
2018-04-20 22:17:19 +0300

    Adding configuration unix: NODE_ENV=staging node index.js win: set NODE_ENV=staging&&node index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
 
 // The server should respond to all requests with a string
 // На каждый запрос вызывается колбэк с параметрами из запроса
@@ -76,9 +77,9 @@ var server = http.createServer(function(req, res) {
     });
 });
 
-// Start the server, have it listen on port 3000
-server.listen(3000, function() {
-    console.log("The server is listening on port 3000 now");
+// Start the server
+server.listen(config.port, function() {
+    console.log("The server is listening on port " + config.port + " in " + config.envName + " mode");
 });
 
 // Define handlers

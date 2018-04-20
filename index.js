830adbd1541d28b0b8ba3482401a6816bd8779dc
SpaceHead1C <damh17@gmail.com>
2018-04-20 22:28:10 +0300

    Background workers Refactoring

--- index.js
+++ index.js
@@ -4,120 +4,24 @@
  */
 
 // Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var _data = require('./lib/data');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
-
-// @TODO GET RID OF THIS
-helpers.sendTwilioSms('4158375309', 'hello', function(err) {
-    console.log('This was the error', err);
-});
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
 
-// The server should respond to all requests with a string
-// На каждый запрос вызывается колбэк с параметрами из запроса
-// req и res хранят информацию о запросе и пользователе
+// Declare the app
+var app = {};
 
-// Instantiate the HTTP server
-var httpServer = http.createServer(function(req, res) {
-    unifiedServer(req, res);
-});
+// Init function
+app.init = function() {
+    // Start the server
+    server.init();
 
-// Start the HTTP server
-httpServer.listen(config.httpPort, function() {
-    console.log("The server is listening on port " + config.httpPort + " in " + config.envName + " mode");
-});
+    // Start the workers
+    workers.init();
+}
 
-// Instatiate the HTTPS server
-var httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions, function() {
-    unifiedServer(req, res);
-});
+// Execute
+app.init();
 
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort, function() {
-    console.log("The server is listening on port " + config.httpsPort + " in " + config.envName + " mode");
-});
-
-// All the server logic for both the http and https server
-var unifiedServer = function(req, res) {
-    // Get the URL and parse it
-    var parsedUrl = url.parse(req.url, true);
-
-    // Get the path
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    // Get the query string as an object
-    var queryStringObject = parsedUrl.query; // look at 'true' parameter for url.parse()
-
-    // Get the HTTP method
-    var method = req.method.toLowerCase();
-
-    // Get the headers as an object
-    var headers = req.headers;
-
-    // Get the payload, if any
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function(data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function() {
-        buffer += decoder.end();
-
-        // Choose the handler this request should go to.
-        // If one is not found< use the notFound handler
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        // Construct the data object to send to the handler
-        var data = {
-            'trimmedPath': trimmedPath,
-            'queryStringObject': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
-
-        // Route the request to the handler specified in the router
-        chosenHandler(data, function(statusCode, payload) {
-            // Use the status code called back by the handler, or default to 200
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-            // Use the payload called back by the handler, or default to an empty object
-            payload = typeof(payload) == 'object' ? payload : {};
-
-            // Convert the payload to a string
-            var payloadString = JSON.stringify(payload);
-
-            // Return the response
-            res.setHeader('Content-type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-
-            //Log the request data
-            console.log('Request recieved on path ' + trimmedPath + ' with method ' + method + 
-                ' and with these query string parameters:', queryStringObject);
-            console.log('Request received with these headers:', headers);
-            console.log('Request received with this payload:', buffer);
-            console.log('Returnin this response:', statusCode, payloadString);
-        });
-    });
-};
-
-// Define a request router
-var router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks
-};
+// Export the app
+module.exports = app;
